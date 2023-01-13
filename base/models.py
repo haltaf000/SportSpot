4c01f30c24fa6558d7709aac4f7e1eac0bcb58f8
Your Name <haider.altaf1996@gmail.com>
2023-01-12 21:53:15 -0500

    manage some migrations

--- base/models.py
+++ base/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
-    cricket_praticipant = models.BooleanField(default='True', null=True)
+    cricket_praticipant = models.BooleanField(default=True, null=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
