f5bbdf58ae5f7855c72a04bec5302aa5a26374a2
Your Name <haider.altaf1996@gmail.com>
2023-01-12 21:48:33 -0500

    fixed migrations

--- base/models.py
+++ base/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import uuid
 # Create your models here.
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
-    cricket_praticipant = models.BooleanField(default=True, null=True)
+    cricket_praticipant = models.BooleanField(default='True', null=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
