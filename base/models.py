76caa9e118a9227783b7bfa9dbe943f5c55f927b
Your Name <haider.altaf1996@gmail.com>
2023-01-12 23:26:33 -0500

    all

--- base/models.py
+++ base/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import time
 from datetime import datetime
-import uuid
 # Create your models here.
 
 class User(AbstractUser):
