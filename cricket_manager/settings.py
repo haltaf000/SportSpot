f23b0c75d878855766d5859b1fe933098d5814d9
Your Name <haider.altaf1996@gmail.com>
2023-01-12 22:15:34 -0500

    made changes

--- cricket_manager/settings.py
+++ cricket_manager/settings.py
@@ -33,10 +33,10 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
 
-#ALLOWED_HOSTS = ['web-production-c5da.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-5d4d.up.railway.app', '127.0.0.1']
 
 
-#CSRF_TRUSTED_ORIGINS = ['https://web-production-c5da.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-5d4d.up.railway.app']
 
 
 AUTH_USER_MODEL = 'base.User'
