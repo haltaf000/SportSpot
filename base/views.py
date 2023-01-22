4d33bf13d19dee19511b92d0084144cefb151547
Your Name <haider.altaf1996@gmail.com>
2023-01-21 23:57:08 -0500

    something that should work

--- base/views.py
+++ base/views.py
@@ -8,7 +8,6 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from dateutil.tz import gettz
 
 
 
